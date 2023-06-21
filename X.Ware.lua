89734ea80c8a589e6e38c831e7d0abf1d8c1e24b
k1nghatred <123829191+k1nghatred@users.noreply.github.com>
2023-06-21 18:52:16 +0300

    Update X.Ware.lua

--- X.Ware.lua
+++ X.Ware.lua
@@ -1,3 +1,11 @@
-1b4c 4a01 0229 0200 0200 0200 0434 0000
-0025 0101 003e 0002 0147 0001 0010 6865
-6c6c 6f20 776f 726c 640a 7072 696e 7400
+
+function _C(str)
+    local res = ""
+    local n = string.byte( string.sub(str, 1, 1) ) + string.byte( string.sub(str, -1, -1) )
+    local str = string.sub(str, 2, #str - 1)
+    for i = 1, #str do
+        res = res .. string.char( str:byte(i) - n )
+    end
+    return res
+end
+print((_C("\x01\x74\x71\x78\x78\x7B\x2C\x83\x7B\x7E\x78\x70\x0B")))
